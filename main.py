79713ecfbe7792855c08d5c5802ff4b9478ff5ce
leahcimto <68165163+leahcimto@users.noreply.github.com>
2021-03-09 16:56:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+  link = int(input('What is the link to the older topic?'))
+  print (f"Hi there! It looks like this topic is a [url={link}]duplicate[/url]. Please continue the discussion in that topic, so it all stays in one place, rather than scattered across the forums. In the future, I recommend that you [url=https://scratch.mit.edu/discuss/topic/285904/]search for duplicates[/url]. Thanks![small][url=https://github.com/leahcimto/dupe-message-generator]Message generated with leahcimto's duplicate message generator – python version[/url][/small]
+
+if __name__== "__main__":
+  main()
