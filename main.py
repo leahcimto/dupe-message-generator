b4fe904b1c0aec9a0cef925e03bb55c8d2b4dc2d
leahcimto <68165163+leahcimto@users.noreply.github.com>
2021-03-09 17:12:21 -0500

    cool changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
   link = (input('What is the link to the older topic?'))
+  print('\n')
+  print("Copy the following BBCode and paste it on your post:")
+  print('\n'*3)
   print (f"Hi there! It looks like this topic is a [url={link}]duplicate[/url]. Please continue the discussion in that topic, so it all stays in one place, rather than scattered across the forums. In the future, I recommend that you [url=https://scratch.mit.edu/discuss/topic/285904/]search for duplicates[/url]. Thanks![small][url=https://github.com/leahcimto/dupe-message-generator]Message generated with leahcimto's duplicate message generator – python version[/url][/small]")
 
 if __name__== "__main__":
